7cd428cb42af0e090f83e327491af83b3b9d350a
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-08-09 20:32:37 +0200

    init

---
+++ src/com/company/Player.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class Player extends GameObject {
+
+    Random r = new Random();
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+
+        velX = r.nextInt(5) + 1;
+        velY = r.nextInt(5);
+    }
+
+    @Override
+    public void tick() {
+        x += velX;
+        y += velY;
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.green);
+        g.fillRect(x, y, 32, 32);
+
+    }
+}
