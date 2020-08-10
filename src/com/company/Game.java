93e6a2acadedb5fa8f1a110880392daa90109c98
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-08-10 22:36:55 +0200

    Splash test

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -31,6 +31,7 @@ public class Game extends Canvas implements Runnable {
         handler.addObject(new Player(WIDTH / 2 - 32, HEIGHT / 2 - 32, ID.Player));
         handler.addObject(new Player(WIDTH / 2 + 64, HEIGHT / 2 - 32, ID.Player2));
         handler.addObject(new BasicEnemy(WIDTH / 2 + 64, HEIGHT / 2 - 32, ID.BasicEnemy));
+        handler.addObject(new Splash(WIDTH / 2, HEIGHT / 2 - 32, ID.Splash));
 
     }
 
---
+++ src/com/company/Splash.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Splash extends GameObject {
+
+    public Splash(int x, int y, ID id) {
+        super(x, y, id);
+
+        velX = 1;
+        velY = -velX*2;
+    }
+
+    @Override
+    public void tick() {
+        x += velX;
+        y += velY;
+
+        if(y <= 0 || y >=Game.HEIGHT-32) velY *= -1;
+        if(x <= 0 || x>=Game.WIDTH-16) velX *= -1;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillRect(x,y,10,10);
+    }
+
+}
